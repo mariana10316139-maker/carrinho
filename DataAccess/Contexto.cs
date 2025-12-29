4bff6c5d3f03c1087f7c45bcda49b0662ea50aa9
Gabriel Ruas <gabrielruas30@gmail.com>
2025-12-29 17:42:12 -0300

    Criando Contexto

---
+++ DataAccess/Contexto.cs
@@ -0,0 +1,19 @@
+namespace DataAcess;
+
+public class Contexto : DbContext
+{
+    public DbSet<Panha> Panha {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=DbProCafeiro;User ID=Sa; Password=Senha@123;Encrypt=False;TrustServerCertificate=True;"
+        );
+    }
+
+    protected override void OnModelCreating(OnModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new PanhaConfiguration());
+    }
+}
+    
